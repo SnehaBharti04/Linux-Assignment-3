b3121ab441ff322f751f310581839e2f5284627c
SnehaBharti04 <83042194+SnehaBharti04@users.noreply.github.com>
2022-03-24 23:47:05 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+pthread_t tid;
+
+void * t1(void *arg){
+    printf("newly created thread is executing\n");
+    int s;
+    s = pthread_cancel(tid);
+    pthread_t ttid = pthread_self();
+    if(s==0){ 
+        printf("cancel thread %u\n", ttid);
+    }
+    return NULL;
+}
+
+int main(){
+    int ret = pthread_create(&tid, NULL, t1, NULL);
+    int rc, t;
+
+
+    if(ret){
+        printf("thread is not created\n");
+    } else{
+        printf("thread is created, thread ID = %u\n", tid);
+        pthread_join(tid, NULL); 
+    }
+    return 0;
+}
